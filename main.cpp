84798cdf8df13a1719df6f036b8c1679c710c254
koziacz <mikolajkludkiewicz@gmail.com>
2018-11-09 20:29:41 +0100

    zatwierdzone i zraportowane okna

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 
+#include <windows.h>
 #include <iostream>
 using std::cout;
 using std::cin;
+using std::string;
 /**
-Dzień dobry. Mam na imię mikołaj. Zająłem się tu ramkami i ich wymiarami (wymiary nie są dopasowane do okna #wersjabeta). Same wymiary okien trzeba przeskalować,
-żeby wyglądały legitnie.
-*/
+Dzień dobry. Mam na imię mikołaj.
+Wymiary okien:
+    - Marginesy: 29
+    - Górne okno: 70x8
+    - Dolne okno: 70x30
+/*
 
 
-    /*
-        179 poj
-        196
+        186 podw. pionowa
+        205 podw. pozioma
+        201 podw. lewy górny róg
+        187 podw. prawy górny róg
+        200 podw. lewy dolny róg
+        188 podw. prawy dolny róg
 
-
-        186 podw
+        196 poj pozioma
+        179 poj pionowa
         218 lewy górny róg
         191 prawy górny róg
         192 lewy dolny róg
@@ -22,84 +30,87 @@ Dzień dobry. Mam na imię mikołaj. Zająłem się tu ramkami i ich wymiarami (
 
 int main (){
 
+char ppo = 196; //ppo  - pojedynzca pozioma
+char ppi = 179; //ppi - pojedyncza pionowa
+char lgr = 218; //lgr - lewy górny róg
+char pgr = 191; //pgr - prawy górny róg
+char ldr = 192; //ldr - lewy dolny róg
+char pdr = 217; //pdr - prawy dolny róg
 
-for (int x=0 ;x<20 ;++x) {                 //górna kreska górnej ramki
-    cout << " ";
-}
-cout << (char) 218;
+string margin29 = "                             ";
+
+                 //górna kreska górnej ramki
+    cout << margin29;
+
+cout << lgr;    //lewy górny róg
 for (int x=0 ;x<68 ;++x) {
-    cout << (char) 196;
+    cout << ppo;   //pojedyncza pozioma
 }
-cout << (char) 191;
-for (int x=0 ;x<20 ;++x) {
-    cout << " ";
-}
-
+cout << pgr;  //prawy górny róg
 
 
 
 cout << "\n";  //debug pierwszego wiersza bocznych kresek
 
-for (int x=0 ;x<20 ;++x) {         //boczne kreski górnej ramki
-    for (int x=0 ;x<20 ;++x) {
-        cout << " ";
-    }
-    cout << (char)179;
+for (int x=0 ;x<8 ;++x) {         //boczne kreski górnej ramki
+
+        cout << margin29;
+
+    cout << ppi;  //pojedyncza pionowa
     for (int x=0; x<68 ;x++) {
         cout << " ";
     }
-    cout << (char)179;
+    cout << ppi;  //pojedyncza pionowa
     cout << "\n";
 }
 
-for (int x=0 ;x<20 ;++x) {        //dolna kreska górnej ramki
-    cout << " ";
-}
-cout << (char) 192;
+         //dolna kreska górnej ramki
+    cout << margin29;
+
+cout << ldr; //lewy dolny róg
 for (int x=0 ;x<68 ;++x) {
-    cout << (char) 196;
+    cout << ppo;   //pojedyncza pionowa
 }
-cout << (char) 217;
+cout << pdr;    //prawy dolny róg
 
 
 
 cout << "\n";             //przerwa między ramkami
 
+//####################################################################################################################
 
-
-for (int x=0 ;x<20 ;++x) {                 //górna kreska dolnej ramki
-    cout << " ";
-}
-cout << (char) 218;
+                 //górna kreska dolnej ramki
+    cout << margin29;
+cout << (char) lgr;  //lewy górny róg
 for (int x=0 ;x<68 ;++x) {
-    cout << (char) 196;
+    cout << ppo;   //pionowa pojdyncza
 }
-cout << (char) 191;
-for (int x=0 ;x<20 ;++x) {
-    cout << " ";
-}
+cout << pgr;   //prawy górny róg
+
 
 cout << "\n";  //debug pierwszego wiersza bocznych kresek
 
-for (int x=0 ;x<35 ;++x) {         //boczne kreski górnej ramki
-    for (int x=0 ;x<20 ;++x) {
-        cout << " ";
-    }
-    cout << (char)179;
+for (int x=0 ;x<30 ;++x) {         //boczne kreski dolnej ramki
+
+        cout << margin29;
+
+    cout << ppi;  //pojedyncza pionowa
     for (int x=0; x<68 ;x++) {
         cout << " ";
     }
-    cout << (char)179;
+    cout << ppi;  //pojedyncza pionowa
     cout << "\n";
 }
 
-for (int x=0 ;x<20 ;++x) {        //dolna kreska górnej ramki
-    cout << " ";
-}
-cout << (char) 192;
+        //dolna kreska dolnej ramki
+    cout << margin29;
+
+cout << ldr;  //lewy dolny róg
 for (int x=0 ;x<68 ;++x) {
-    cout << (char) 196;
+    cout << ppo;  //pojedyncza pozioma
 }
-cout << (char) 217;
+cout << pdr;  //prawy dolny róg
+cout << "\n";
+system("pause");
     return 0;
 }
