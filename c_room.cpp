da16587a27c20c4c101c3dc13b5fa608ca4b2628
grzybowb <b.grzybowski.20@gmail.com>
2018-11-10 23:12:09 +0100

    Klasa hotel i metoda SetRoom (niedokończona)

--- c_room.cpp
+++ c_room.cpp
@@ -1,4 +1,3 @@
-
 room::room MakeRoom(int lvl,int wid ,room* n,room* e,room* s,room* w ) { ///tworzy jeden pokoj, jesli bez argumentow - pokoj 00
 			room room;
 
@@ -10,8 +9,7 @@ room::room MakeRoom(int lvl,int wid ,room* n,room* e,room* s,room* w ) { ///twor
 			room.west = w;
 			room.Me = this;
 
-			if ((lvl==0)&&(wid==0)){
-                     hotel.root=room;
+
             }
 
 			return room;
@@ -25,30 +23,38 @@ room::void PrintRoom(room room){
 		cout<<room.west<<endl;
 	}
 
-room :: room SetRoom(room room){ /// tworzenie nowych roomow, losowanie jakich i ilu, argumenty to obecny pokoj
+room :: hotel SetRoom(room room){ /// tworzenie nowych roomow, losowanie jakich i ilu, argumenty to obecny pokoj, zwraca pudelo z pokojami w srodku
 		srand(time(NULL));
 		int x = std::rand()%13 -6;
 
 		if ( x==0) {
+                hotel h;
                 room room0 = MakeRoom(room.lvl + 1,room.wid,NULL,NULL,room.Me,NULL);
-					return room0;
+                h = MakeHotel(&room0);
+					return h;
 		}
 		if( x==1){
+                hotel h;
                 room room0 = MakeRoom(room.lvl,room.wid + 1,NULL,NULL,NULL,room.Me);
                 room room1 = MakeRoom(room.lvl + 1,room.wid + 1,NULL,NULL,room0.Me,NULL);
-                    return room1;
+                h = MakeHotel(&room0,&room1);
+                    return h;
             }
         if ( x==2) {
+                hotel h;
                 room room0 = MakeRoom(room.lvl,room.wid + 1,NULL,NULL,NULL,room.Me);
 				room room1 = MakeRoom(room.lvl,room.wid + 2,NULL,NULL,NULL,room0.Me);
 				room room2 = MakeRoom(room.lvl + 1 ,room.wid + 2,NULL,NULL,room1.Me,NULL);
-                    return room2;
+                h = MakeHotel(&room0,&room1,&room2);
+                    return h;
         }
         if (x==3){
+                hotel h;
                 room room0 = MakeRoom(room.lvl,room.wid - 1,NULL,room.Me,NULL,NULL);
 				room room1 = MakeRoom(room.lvl,room.wid + 1,NULL,NULL,NULL,room0.Me);
 				room room2 = MakeRoom(room.lvl + 1 ,room.wid + 1,NULL,NULL,room1.Me,NULL);
-                    return room2;
+                h = MakeHotel(&room0,&room1,&room2);
+                    return h;
         }
 
 
