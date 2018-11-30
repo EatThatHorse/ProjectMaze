bdda8f1dee9a662f6f42c6fa351a3a35e8b7bcea
Kacu <k.ptychu@gmail.com>
2018-12-01 00:55:11 +0100

    nowy folder PRM_CORE, Menu Na szybko

---
+++ PRM_Core/GreatLoop.cpp
@@ -0,0 +1,89 @@
+
+#include "c_GreatLoop"
+
+
+void DEBUG_KEYBOARD ();
+
+
+void GreatLoop::Execute(){
+    /** Czynnosci wykonywane przez Program.
+        Obsluga wiekszosci bledow.
+        Metoda wywolywana w MAIN().
+    */
+    //#define PRM__DEBUG_INPUT    // UnComment To Enable DEBUG_INPUT
+    #ifdef PRM__DEBUG_INPUT
+    {       /// When Active: No Game is runnig. Just DEBUG_INPUT.
+        while(1) DEBUG_KEYBOARD();
+    }
+    #else
+    // _________________
+    /// STARTING POINT:
+
+    //GameVer01();
+    MenuVer01();
+
+    #endif
+    return;
+}
+
+
+void GreatLoop::GameVer01() {
+    /** Pierwsza wersja Funkcji Sterującej Rozgrywką. */
+
+    player HERO;
+
+    GLOOP = true;           // GREAT LOOP - Wykonywana tak długo jak działa program.
+    while (GLOOP) {
+        //   Jedno Wykonanie Petli = Jeden Input, Jeden Klawisz = Jedna Klatka, Jeden Frame.
+
+        int InputEvent = this->Input::Run01G(HERO);
+
+        if (InputEvent != 224) {    // unikamy zdarzenia 224. Zawsze. Wynika to z dzialania [getch] <conio.h>
+            system("cls");                  // Czyszczenie, Zerowanie Konsoli.
+            this->Update::Run01G(HERO);      // Zmiany logiczne na planszy.
+            this->Render::Run01G(HERO);      // Render. Aktywowany Tyle razy ile cokolwiek się zmieni na ekranie.
+        }
+    }
+    return;
+}
+
+
+void GreatLoop::MenuVer01() {
+    /** Pierwsza wersja Funkcji Sterującej Rozgrywką. */
+
+    int currentChoose = 0;
+
+    this->Render::Run01M(currentChoose); // PreRender, Rysowanie Pierwszej Klatki.
+
+    GLOOP = true;           // GREAT LOOP - Wykonywana tak długo jak działa program.
+    while (GLOOP) {
+        //   Jedno Wykonanie Petli = Jeden Input, Jeden Klawisz = Jedna Klatka, Jeden Frame.
+
+        int InputEvent = this->Input::Run01M(GLOOP, currentChoose);
+
+        if (InputEvent != 224) {    // unikamy zdarzenia 224. Zawsze. Wynika to z dzialania [getch] <conio.h>
+            system("cls");                          // Czyszczenie, Zerowanie Konsoli.
+            this->Render::Run01M(currentChoose);    // Render. Aktywowany Tyle razy ile cokolwiek się zmieni na ekranie.
+        }
+    }
+    return;
+}
+
+
+void DEBUG_KEYBOARD (){
+    /** Funkcja Pokazujaca w Konsoli Wcisniete Klawisze. */
+
+    unsigned char x = getch();
+    if (x != 224) {
+        system("cls");    // Czyszczenie, Zerowanie Konsoli.
+        cout << "\n MODE: DEBUG_INPUT \n"
+             << "  - no game's running\n\n"
+             << " Key Pressed:\n"
+             << "  (unsigned char): " << x
+             << "  (int): "           << (int)x
+             << "\n\n";
+    }
+}
+
+
+
---
+++ PRM_Core/Update.cpp
@@ -0,0 +1,11 @@
+
+#include "c_Update"
+
+
+
+
+void Update::Run01G (player HERO){
+    /// FINAL UPDATE OF RENDER_TAB:
+
+    return;
+}
