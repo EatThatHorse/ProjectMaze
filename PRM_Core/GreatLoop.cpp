455bbdfc1670505ca490306b60eb945bfbfef047
Kacu <k.ptychu@gmail.com>
2018-12-15 17:31:58 +0100

    działa c_record i c_container

--- PRM_Core/GreatLoop.cpp
+++ PRM_Core/GreatLoop.cpp
@@ -34,7 +34,7 @@ void GreatLoop::Execute(){
 
     //GameVer01();
     //GameVer02();
-    GrzyboVer01();
+    //GrzyboVer01();
 
 
 
@@ -42,7 +42,76 @@ void GreatLoop::Execute(){
     return;
 }
 
+
+#include "../c_Container"
+#include <iostream>
+using namespace std;
+
+
 void GreatLoop::MAIN_TEST_DEBUG() {
+
+
+
+/*
+
+    Container testTab;
+
+    // TEST 0 ----------------------------------
+    cout << "\n TEST 0 - konstruktor kontenera. Brak modyfikacji."
+         << "\n --------------------------------------------------------------------\n";
+    testTab.ShowKontener(); cout << "\n";
+
+    // TEST 1 ----------------------------------
+    testTab.AddRecord (2,2,'A');
+    testTab.AddRecord (3,4,'B');
+    testTab.AddRecord (6,7,'C');
+    testTab.AddRecord (8,5,'D');
+    cout << "\n TEST 1 - Dodawanie 4 Rekordow"
+         << "\n --------------------------------------------------------------------\n";
+    testTab.ShowKontener(); cout << "\n";
+
+    // TEST 2 --------------------------------------------------------------------
+    testTab.DeleteLastRecord ();
+    testTab.DeleteLastRecord ();
+    cout << "\n TEST 2 - Usuwanie 2 Rekordow"
+         << "\n --------------------------------------------------------------------\n";
+    testTab.ShowKontener(); cout << "\n";
+
+    // TEST 3 --------------------------------------------------------------------
+    testTab.AddRecord (0,0,'K');
+    testTab.AddRecord (0,0,'K');
+    testTab.AddRecord (0,0,'K');
+    cout << "\n TEST 3 - Ponowne Dodawanie 3 Rekordow"
+         << "\n --------------------------------------------------------------------\n";
+    testTab.ShowKontener(); cout << "\n";
+
+    // TEST 4 --------------------------------------------------------------------
+    testTab.DeleteKontener();
+    cout << "\n TEST 4 - Kasowanie Wszystkich Rekordow"
+         << "\n --------------------------------------------------------------------\n";
+    testTab.ShowKontener(); cout << "\n";
+
+    // TEST 5 --------------------------------------------------------------------
+    testTab.DeleteLastRecord ();
+    testTab.DeleteLastRecord ();
+    testTab.DeleteLastRecord ();
+    testTab.DeleteLastRecord ();
+    testTab.DeleteLastRecord ();
+    cout << "\n TEST 5 - Kasowanie Jeszcze 5, Nie Istniejacych Rekordow"
+         << "\n --------------------------------------------------------------------\n";
+    testTab.ShowKontener(); cout << "\n";
+
+    // TEST 6 --------------------------------------------------------------------
+    testTab.AddRecord (2,2,'A');
+    testTab.AddRecord (3,4,'B');
+    testTab.AddRecord (0,0,'K');
+    testTab.AddRecord (7,9,'Z');
+    cout << "\n TEST 6 - Dodawanie Do Pustego Kontenera 4 Rekordow"
+         << "\n --------------------------------------------------------------------\n";
+    testTab.ShowKontener(); cout << "\n";
+
+*/
+
 
 }
 
