d7dd83cd44a007f1f4cf00d03b3fb97bbcb9f188
Ptysiek <k.ptychu@gmail.com>
2018-12-19 13:56:40 +0100

    started with PRM_Mazegen

---
+++ PRM_MazeGen/c_NavRoom.cpp
@@ -0,0 +1,64 @@
+
+#include "c_NavRoom"
+
+// ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
+
+int room::iloscPokoi = 0; //statyczna zmienna globalna
+
+// ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
+room::room(room* t_south,room* t_east,room* t_west ,int t_lvl,int t_wid){
+
+    ++iloscPokoi;
+
+    this->south = t_south;
+    this->north = NULL;
+    this->east = t_east;
+    this->west = t_west;
+    this->lvl = t_lvl;
+    this->wid = t_wid;
+    this->nr = iloscPokoi;
+
+    if (t_south != NULL)
+        t_south->north = this;
+}
+// ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
+room::~room (){
+    --iloscPokoi;
+}
+// ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
+
+int room::LVL(){ return this->lvl; }
+int room::WID(){ return this->wid; }
+int room::NR(){ return this->nr; }
+
+room* room::NORTH(){ return this->north; }
+room* room::EAST(){ return this->east; }
+room* room::SOUTH(){ return this->south; }
+room* room::WEST(){ return this->west;
+}
+// ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
+void room::SET_NORTH(room* roomX){
+    this->north = roomX;
+}
+
+void room::SET_EAST(room* roomX){
+    this->east = roomX;
+}
+
+void room::SET_WEST(room* roomX){
+    this->west = roomX;
+}
+// ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
+void room::DEBUG_SHOWVARS(){
+    cout <<(char)254
+         << " lvl: "            << this->lvl
+         << " wid: "            << this->wid
+         << " nr: "             << this->nr
+         << "\t\tthis: ["       << this
+         << "] N: ["        << this->north
+         << "] S: ["        << this->south
+         << "] E:  ["        << this->east
+         << "] W:  ["        << this->west
+         << "]\n";
+}
+// ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■
